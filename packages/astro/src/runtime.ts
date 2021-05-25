4adc8cfdad1d4fa976c471d20c79761dc6e12d4d
Nate Moore <nate@skypack.dev>
2021-05-25 18:07:12 -0500

    wip: add optimizeHtml step

--- packages/astro/src/runtime.ts
+++ packages/astro/src/runtime.ts
@@ -12,6 +12,7 @@ import { loadConfiguration, logger as snowpackLogger, startServer as startSnowpa
 import { canonicalURL, stopTimer } from './build/util.js';
 import { debug, info } from './logger.js';
 import { searchForPage } from './search.js';
+import { optimizeHtml } from './optimize/index.js';
 
 // We need to use require.resolve for snowpack plugins, so create a require function here.
 import { createRequire } from 'module';
@@ -218,14 +219,7 @@ async function load(config: RuntimeConfig, rawPathname: string | undefined): Pro
       props: { collection },
     })) as string;
 
-    // inject styles
-    // TODO: handle this in compiler
-    const styleTags = Array.isArray(mod.css) && mod.css.length ? mod.css.reduce((markup, href) => `${markup}\n<link rel="stylesheet" type="text/css" href="${href}" />`, '') : ``;
-    if (html.indexOf('</head>') !== -1) {
-      html = html.replace('</head>', `${styleTags}</head>`);
-    } else {
-      html = styleTags + html;
-    }
+    html = await optimizeHtml(html, { css: mod.css });
 
     return {
       statusCode: 200,
